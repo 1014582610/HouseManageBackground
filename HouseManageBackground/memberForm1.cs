439757533425cac7f2f4ce7703e9095abe9143bd
林宜葳 <lin1014582610@qq.com>
2013-07-23 00:06:42 +0800

    _2

--- HouseManageBackground/memberForm1.cs
+++ HouseManageBackground/memberForm1.cs
@@ -11,6 +11,11 @@ namespace HouseManageBackground
 {
     public partial class memberForm1 : Form
     {
+        int pageSize = 0;
+        int pageMaxSize = 0;
+        int pageCount = 0;
+        int pageCurrent = 0;
+        int lineCurrent = 0;
         public memberForm1()
         {
             InitializeComponent();
@@ -19,8 +24,25 @@ namespace HouseManageBackground
         private void memberForm1_Load(object sender, EventArgs e)
         {
             // TODO: 这行代码将数据加载到表“housemanagementDataSet.person”中。您可以根据需要移动或删除它。
-            this.personTableAdapter.Fill(this.housemanagementDataSet.person);
+            //this.personTableAdapter.Fill(this.housemanagementDataSet.person);
+            InitDataset();
+        }
+        private void InitDataset()
+        {
+            pageSize = 1;
+            pageMaxSize = this.housemanagementDataSet.person.Rows.Count;
+            pageCount = (pageMaxSize / pageSize);
+            if ((pageMaxSize % pageSize) > 0)
+            {
+                pageCount++;
+            }
+            pageCurrent = 1;
+            lineCurrent = 0;
 
+        }
+        private void LoadData()
+        {
+ 
         }
     }
 }
